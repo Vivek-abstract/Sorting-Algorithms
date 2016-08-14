dc407263c9483130359f653c452524c6afa46185
Vivek Gawande <vivek040997@gmail.com>
2016-08-14 20:51:08 +0530

    Decimal to Binary is epic!

---
+++ DecimalToBinary.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX 100
+int Binary(int n);
+int stack[MAX];
+int top = -1;
+int main(){
+    int n, b;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    Binary(n);
+    return 0;
+}
+int Binary(int n){
+    int r;
+    while(n > 0){
+        stack[++top] = n % 2;
+        n /= 2;
+    }
+    while(top != -1){
+        printf("%d", stack[top--]);
+    }
+}
